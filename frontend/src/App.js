344344e300037896f69bf609252a108bdc8cfe0f
luucamay <lupemaydana@gmail.com>
2020-01-24 17:14:54 -0400

    Simplifying the project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Registro de gastos e ingresos</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          <h2>Transacciones</h2>
+        </div>
+      </div>
     </div>
   );
 }
